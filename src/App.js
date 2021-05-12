8a94219d0a2b22f415eadbe35b6f9b3a80f0d9a4
Josh <johillzm@amazon.com>
2021-05-12 11:40:00 -0400

    restructured

--- src/App.js
+++ src/App.js
@@ -1,29 +1,9 @@
 import React from 'react';
-import { AmplifyAuthenticator, AmplifySignUp, AmplifySignIn } from '@aws-amplify/ui-react';
+import Login from "./components/Login"
 
 const App = () => {
   return (
-    <AmplifyAuthenticator usernameAlias="email">
-      <AmplifySignUp
-        slot="sign-up"
-        usernameAlias="email"
-        formFields={[
-          {
-            type: "email",
-            label: "Enter Email",
-            placeholder: "custom email placeholder",
-            required: true,
-          },
-          {
-            type: "password",
-            label: "Enter Password",
-            placeholder: "custom password placeholder",
-            required: true,
-          },
-        ]} 
-      />
-      <AmplifySignIn slot="sign-in" usernameAlias="email" />
-    </AmplifyAuthenticator>
+    <Login />
   );
 };
 
---
+++ src/components/Login.js
@@ -0,0 +1,31 @@
+import React from "react"
+import { AmplifyAuthenticator, AmplifySignUp, AmplifySignIn } from '@aws-amplify/ui-react';
+
+function Login() {
+
+    return (
+        <AmplifyAuthenticator usernameAlias="email">
+         <AmplifySignUp
+          slot="sign-up"
+          usernameAlias="email"
+          formFields={[
+            {
+              type: "email",
+              label: "Enter Email",
+              placeholder: "custom email placeholder",
+              required: true,
+            },
+            {
+              type: "password",
+              label: "Enter Password",
+              placeholder: "custom password placeholder",
+              required: true,
+            },
+          ]} 
+        />
+        <AmplifySignIn slot="sign-in" usernameAlias="email" />
+      </AmplifyAuthenticator>
+    )
+}
+
+export default Login
