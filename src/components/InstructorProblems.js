0e4d350cb7ff6520b8d4314fd2b4ec4c855d1373
Josh <johillzm@amazon.com>
2021-06-01 13:27:41 -0400

    added table

--- src/components/InstructorProblems.js
+++ src/components/InstructorProblems.js
@@ -2,43 +2,51 @@ import React from "react"
 import { Table } from 'react-bootstrap';
 import { Link } from "react-router-dom"
 import poke from "../pics/Poke_receipt.jpg"
+import { DataGrid } from '@material-ui/data-grid';
 
 function InstructorProblems(){
-    const problems = [
-        { name: "Solve this function", description: "05/25/2021", diagram: <image src={poke} alt="poke"/>},
-        { name: "Create this vpc", description: "07/23/2021", diagram: "Not Started" },
-        { name: "Setup DynamoDB", description: "07/21/2021", diagram: "Completed"}
-    ]
+    const columns = [
+        { field: 'id', headerName: 'ID', width: 90 },
+        { field: 'firstName', headerName: 'First name', width: 150 },
+        { field: 'lastName', headerName: 'Last name', width: 150 },
+        {
+          field: 'age',
+          headerName: 'Age',
+          type: 'number',
+          width: 110,
+        },
+        {
+          field: 'fullName',
+          headerName: 'Full name',
+          description: 'This column has a value getter and is not sortable.',
+          sortable: false,
+          width: 160,
+          valueGetter: (params) =>
+            `${params.getValue(params.id, 'firstName') || ''} ${
+              params.getValue(params.id, 'lastName') || ''
+            }`,
+        },
+      ];
 
-    const renderProblem = (problem, index) => {
-        return (
+      const rows = [
+        { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+        { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+        { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+        { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+        { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+        { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+        { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+        { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+        { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+      ];
 
-            <tr key={index}>
-                <td>{problem.name}</td>
-                <td>{problem.description}</td>
-                <td>{problem.diagram}</td>
-            </tr>
-        )
-    }
 
     return (
         <div>
             <Link to="problems/create-problem">
                 <button>Create Problem</button>
             </Link>
-
-            <Table striped bordered hover>
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>description</th>
-                        <th>diagram</th>
-                    </tr>
-                </thead>
-                <tbody>
-                   {problems.map(renderProblem)}
-                </tbody>
-            </Table>
+            <DataGrid rows={rows} columns={columns} pageSize={5} checkboxSelection />
         </div>
     )
 }
