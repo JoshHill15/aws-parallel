b7f7411e5a017ae2bfc43d99c8faa0bea0255ec3
Josh <johillzm@amazon.com>
2021-06-09 15:38:12 -0400

    student table dynamic complete

--- src/components/InstructorsStudents.js
+++ src/components/InstructorsStudents.js
@@ -2,36 +2,30 @@ import React, { useEffect, useState } from "react"
 import { DataGrid } from '@material-ui/data-grid';
 import { API, Auth } from "aws-amplify"
 
-  const rows = [
-    { id: 1, studentName: 'd', problemName: 'desc', submission: "link to submission"},
-    { id: 2, studentName: 'why ', problemName: 'Cersei', submission: 42 },
-    { id: 3, studentName: 'Lannister', problemName: 'Jaime', submission: 45 },
-    { id: 4, studentName: 'Stark', problemName: 'Arya', submission: 16 },
-    { id: 5, studentName: 'Targaryen', problemName: 'Daenerys', submission: null },
-    { id: 6, studentName: 'Melisandre', problemName: null, submission: 150 },
-    { id: 7, studentName: 'Clifford', problemName: 'Ferrara', submission: 44 },
-    { id: 8, studentName: 'Frances', problemName: 'Rossini', submission: 36 },
-    { id: 9, studentName: 'Roxie', problemName: 'Harvey', submission: 65 },
-  ];
 
 function InstructorsStudents(){
     const [email, setEmail] = useState("")
-    console.log("component loaded")
+    const [rows, setRows] = useState([])
 
     Auth.currentAuthenticatedUser()
         .then(data => setEmail(data.username))
         .catch(err => console.log(err))
 
     async function getStudentProblems() {
-        console.log("SDJ")
         const myInit = {
             queryStringParameters: {
                 instructor_email: email
             }
         }
         try {
-            const res = await API.get("studentProblems", "/studentProblems/:instructor_email", myInit)
+            let count = 1
+            let res = await API.get("studentProblems", "/studentProblems/:instructor_email", myInit)
+            res = res.map(cv => {
+                cv.id = count++
+                return cv
+            })
             console.log("Student",res)
+            setRows(res)
         } catch (e) {
             console.log("errrr", e)
         }
@@ -39,14 +33,15 @@ function InstructorsStudents(){
     }
 
     const columns = [
-        { field: 'id', headerName: 'ID', width: 125 },
-        { field: 'studentName', headerName: 'Students Name', width: 350 },
-        { field: 'problemName', headerName: 'Problem Name', width: 350 },
-        { field: 'submission', headerName: 'Submission', width: 350 }
+        { field: 'id', headerName: 'ID', width: 100 },
+        { field: 'studentsName', headerName: 'Students Name', width: 250 },
+        { field: 'problemName', headerName: 'Problem Name', width: 250 },
+        { field: 'submission', headerName: 'Submission', width: 250 },
+        { field: 'grade', headerName: 'Grade', width: 250 },
+        { field: 'instructorReview', headerName: 'Review', width: 200 }
       ];
 
     useEffect(() => {
-        console.log("useeffect")
         if (email !== "") getStudentProblems()
     }, [email])
 
